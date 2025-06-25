8cd7b76c37d0989944527a193301186511c7356d
sumeyye <sumeyyekirtil4@gmail.com>
2025-06-25 22:02:56 +0300

    örnekler, StringBuilder, List

--- Konu16CollectionsKoleksiyonlar/Program.cs
+++ Konu16CollectionsKoleksiyonlar/Program.cs
@@ -12,7 +12,13 @@ namespace Konu16CollectionsKoleksiyonlar
 			//Ornek1();//main metotunda çağırıldı
 			//Ornek2();
 			//Ornek3();
-			Ornek4();
+			//Ornek4();
+			//Ornek5();
+			//Ornek6();
+			//Ornek7();
+			//Ornek8();
+			//StringBuilderKullanimi();
+			ListKullanimi();
 		}
 		static void Ornek1()
 		{
@@ -80,6 +86,7 @@ namespace Konu16CollectionsKoleksiyonlar
 		static void Ornek4()
 		{
 			var strings = new StringCollection();
+			//strings.Add(24); //sadece string değer atanabilir
 			strings.Add("İstanbul");
 			strings.Add("Ankara");
 			strings.Add("İzmir");
@@ -88,6 +95,209 @@ namespace Konu16CollectionsKoleksiyonlar
 			foreach (var item in strings)
 			{
 				Console.WriteLine(item);
+			}
+		}
+		static void Ornek5()
+		{
+			var dictionary = new StringDictionary(); //key-value şeklinde veri saklayabilir
+			dictionary.Add("18", "Çankırı");
+			dictionary.Add("24", "Erzincan");
+			dictionary.Add("06", "Ankara");
+			dictionary.Add("34", "İstanbul");
+			dictionary.Add("16", "Bursa");
+			Console.WriteLine();
+			Console.WriteLine("String Dictionary");
+			Console.WriteLine("dictionary 06: " + dictionary["06"]);
+			Console.WriteLine();
+			foreach (var item in dictionary)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine();
+			Console.WriteLine("String Dictionary Keys"); //anahtarları sıralar
+			foreach (var item in dictionary.Keys)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("String Dictionary Values"); //değerleri sıralar
+			foreach (var item in dictionary.Values)
+			{
+				Console.WriteLine(item);
+			}
+		}
+		static void Ornek6()
+		{
+			Stack stack = new(); //Stack sınıfı programlamada LIFO - son giren ilk çıkar kuralıyla çalışır.
+								 // stack.add(); stack de listeye add metoduyla ekleme yapışmaz
+			stack.Push("Çankırı");
+			stack.Push("Ankara");
+			stack.Push("İzmir");
+			stack.Push("Muğla");
+			stack.Push("Denizli");
+			stack.Push(34);
+			stack.Push(56);
+			Console.WriteLine("Stack");
+			while (stack.Count > 0)
+			{
+				Console.WriteLine(stack.Pop()); //stack de nesneler üst üste koyulmuş gibidir verilere de üstten çekerek ulaşılır
+			}
+		}
+		static void Ornek7()
+		{
+			Queue queue = new(); // Queue sınıfı programlamada FIFO- ilk giren ilk çıkar mantığıyla çalışır
+			queue.Enqueue("Lale");
+			queue.Enqueue("Sümbül");
+			queue.Enqueue("Kasımpatı");
+			queue.Enqueue("Zambak");
+			queue.Enqueue(18); //obje aldığı için her türlü veri kabul eder
+			while (queue.Count > 0)
+			{
+				Console.WriteLine(queue.Dequeue());
+			}
+		}
+		static void Ornek8()
+		{
+			Dictionary<string, string> dictionary = new(); //hangi veri tipinden oluşacapını bizim belirlediğimiz liste sistemi
+			dictionary.Add("book", "kitap");
+			dictionary.Add("18", "Çankırı");
+			dictionary.Add("34", "İstanbul");
+			dictionary.Add("06", "Ankara");
+			Console.WriteLine(dictionary["book"]); //book anahtarın değerini getirir
+			Console.WriteLine();
+			Dictionary<int, string> dictionary2 = new(); //int değerler için
+			dictionary2.Add(1, "kitap");
+			dictionary2.Add(18, "Çankırı");
+			dictionary2.Add(34, "İstanbul");
+			dictionary2.Add(06, "Ankara");
+			Console.WriteLine();
+			Console.WriteLine("dictionary2 Values");
+			foreach (var item in dictionary2)
+			{
+				Console.WriteLine(item.Value);
+			}
+			Console.WriteLine();
+			Console.WriteLine("dictionary2 Keys");
+			foreach (var item in dictionary2)
+			{
+				Console.WriteLine(item.Key);
+			}
+			Console.WriteLine();
+			Console.WriteLine("dictionary2 Keys-Values");
+			foreach (var item in dictionary2)
+			{
+				Console.WriteLine(item.Key + " " + item.Value);
+			}
+		}
+		static void StringBuilderKullanimi()
+		{ //String Builder : birleştirmeye yarar
+			Console.WriteLine("String Builder Kullanımı");
+			var sb = new StringBuilder();
+			sb.Append(1); //Append ekleme metodu
+			sb.Append(4);
+			sb.Append(8);
+			Console.WriteLine("sb: " + sb.ToString()); //string leri birleştirir
+			Console.WriteLine();
+
+			StringBuilder sb2 = new();
+			sb2.Append("Ankara");
+			sb2.Append("İstanbul");
+			sb2.Append("Mersin");
+			Console.WriteLine("sb2: " + sb2.ToString());
+			Console.WriteLine();
+			
+			StringBuilder sb3 = new();
+			sb3.Append(" Ankara ");
+			sb3.Append(Environment.NewLine); //Yeni satır ekler arasına
+			sb3.Append(" İstanbul ");
+			sb3.Append(Environment.NewLine);
+			sb3.Append(" Mersin ");
+			sb3.Append(Environment.NewLine);
+			Console.WriteLine("sb3: " + sb3.ToString());
+			Console.WriteLine();
+
+			StringBuilder sb4 = new();
+			sb4.AppendLine("Erzincan"); //satıra ekle metodu: AppendLine
+			sb4.AppendLine("İstanbul");
+			sb4.Append(Environment.NewLine);
+			sb4.AppendLine("Mersin");
+			Console.WriteLine("sb4: " + sb4.ToString());
+			Console.WriteLine();
+			
+		}
+		static void ListKullanimi() //User sınıfı kullanıldı
+		{
+			List<string> sehirler = new(); //string veri tipi alabilen bir liste
+			sehirler.Add("Ankara");
+			sehirler.Add("İstanbul");
+			sehirler.Add("Konya");
+			sehirler.Add("Mersin");
+			Console.WriteLine("Şehirler : ");
+			foreach (var item in sehirler)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine();
+
+			List<User> users = new(); //User içindeki property lere değer atandı
+			users.Add(new User
+			{
+				Id = 1,
+				Name = "Sümeyye",
+				Password = "123"
+			});
+			users.Add(new User
+			{
+				Id = 1,
+				Name = "Selim",
+				Password = "456"
+			});
+			Console.WriteLine("Kullanıcılar: ");
+			foreach (var item in users)
+			{
+				Console.WriteLine(item.Name);
+			}
+			List<User> kullanicilar = new() //liste oluşturmanın bir diğer yöntemi 'dizi gibi'
+			{
+				new User
+				{
+					Id = 3,
+					Name = "Ali",
+					Password = "789"
+				},
+				new User
+				{
+					Id =4,
+					Name = "Sevde",
+					Password = "1071"
+				}
+			};
+			Console.WriteLine();
+			Console.WriteLine("Kullanıcılar2: ");
+			foreach (var kullanici in kullanicilar)
+			{
+				Console.WriteLine(kullanici.Name);
+			}
+			var yenikullanici = new User()
+			{
+				Id = 5,
+				Name = "Alp",
+				Password = "678"
+			};
+			Console.WriteLine("Kullanıcılar listesinde yenikullanici var mı? " + kullanicilar.Contains(yenikullanici)); //Contains metodu yeni değer var ise yazdırır
+			kullanicilar.Add(yenikullanici); //Add ile eklenen yeni üye artık Contains ile true dönebilir ekleme gerçekleşti
+			Console.WriteLine("Kullanıcılar listesinde yenikullanici var mı? " + kullanicilar.Contains(yenikullanici));
+			foreach (var item in kullanicilar)
+			{
+				Console.WriteLine(item.Name);
+			}
+			Console.WriteLine();
+			Console.WriteLine("Listedeki kayıt sayısı : " + kullanicilar.Count); //Count ile toplam veri sayısını alır (değerin boş olup olmadığına bakar)
+																				 //foreach ile açık veriler yazırılabilir
+			Console.WriteLine();
+			kullanicilar.Insert(0, yenikullanici); //Insert metodu ile ekleme yapıldı (index değeri ile birlikte ekleme yapar)
+			foreach (var item in kullanicilar)
+			{
+				Console.WriteLine(item.Name);
 			}
 		}
 	}
