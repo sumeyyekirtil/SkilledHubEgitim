a5f36483e93c9bbda3f8f8ac1d08f01169883175
sumeyye <sumeyyekirtil4@gmail.com>
2025-07-02 21:13:34 +0300

    toolbox nesneleri örnekleri

---
+++ WindowsForms/Form6.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+	public partial class Form6 : Form
+	{
+		public Form6()
+		{
+			InitializeComponent();
+		}
+
+		private void Form6_Load(object sender, EventArgs e)
+		{
+			for (int i = 0; i < 50; i++)
+			{
+				domainUpDown1.Items.Add(FontFamily.Families[i].Name); //domainUpDown1 kontrolüne windowstaki font listesini yüklüyoruz
+			}
+			//ekrandaki combobox a sistemdeki fonr-tları yüklüyoruz 
+			cbFontlar.DataSource = FontFamily.Families;
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			label1.Font = new Font("Arial", (float)numericUpDown1.Value);
+		}
+
+		private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+		{
+			label1.Font = new Font(domainUpDown1.SelectedItem.ToString(),(float)numericUpDown1.Value);
+		}
+
+		private void cbFontlar_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			MessageBox.Show("Seçilen Font : " + cbFontlar.SelectedItem);
+		}
+	}
+}
---
+++ WindowsForms/Form7.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+	public partial class Form7 : Form
+	{
+		public Form7()
+		{
+			InitializeComponent();
+		}
+
+		private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+		{
+			MessageBox.Show("İki tarih arasında " + (dateTimePicker2.Value - dateTimePicker1.Value).Days.ToString() + " gün vardır.");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(textBox1.Text))
+			{
+				int sayi = Convert.ToInt32(textBox1.Text);
+				progressBar1.Maximum = sayi;
+
+				for (int i = 0; i< sayi; i++)
+				{
+					progressBar1.Value = i;
+				}
+			}
+			else
+			{
+				MessageBox.Show("Lütfen Bir Sayı Giriniz!");
+			}
+		}
+	}
+}
