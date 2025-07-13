0689cfe20b31b25c4799caf994a7a8cdcd2378a9
sumeyye <sumeyyekirtil4@gmail.com>
2025-07-13 18:20:05 +0300

    revision and notes added

--- Konu15AbstractClasses/Program.cs
+++ Konu15AbstractClasses/Program.cs
@@ -1,6 +1,11 @@
 ﻿namespace Konu15AbstractClasses
-{//interface ve class ların mix hali 
- //metot imzası oluşturulur ve kalıtım yapılabilir
+{//interface: property-metot imzası tutulur
+ //class: property-metot kendileri tutulur
+ //interface ve class ların mix hali hem interface hem classın yaptığını yapıyor. Abstract sınıflarda interface gibi metot bildirimi yapılabilir ayrıca metot tanımlanabilir.
+ //Bir sınıfa en fazla 1 abstract sınıf inherit(kalıtım) edilebiliyor ancak aynı sınıfta 1 den fazla interface inherit edilebiliyor.
+ //kalıtım yöntemiyle abstract classdan alınıp sınıf oluşturulur ise metot kullanılabilir, new ile yeni nesne oluşturulamaz
+ //TAMAMİYLE kalıtım amaçlı oluşturulmuş sınıf türüdür.
+ //metot imzası oluşturulur ve kalıtım yapılabilir.
 	internal class Program
 	{
 		static void Main(string[] args)
@@ -10,6 +15,7 @@
 			database.Add();
 			database.Delete();
 			Console.WriteLine();
+
 			Database database2 = new SqlServer();
 			database2.Add();
 			database2.Delete();
@@ -21,12 +27,12 @@
 		{
 			Console.WriteLine("Add metodu çalıştı Ekleme yapıldı");
 		}
-		//abstract altına izin veriyor
+		//abstract altına izin veriyor, classlarda izin vermiyor
 		public abstract void Delete();//crud
-		public abstract void Update();
+		public abstract void Update(); //metot imzaları
 		public abstract void Get();
 	}
-	class SqlServer : Database //imlement abstract yapıyor
+	class SqlServer : Database //implement abstract yapıyor
 	{
 		public override void Delete()
 		{
@@ -43,8 +49,8 @@
 			Console.WriteLine("Sql güncellendi");
 		}
 	}
-	class Oracle : Database //imlement abstract yapıyor
-		                    //oracle java altyapısı ile çalışır
+	class Oracle : Database //implement abstract yapıyor
+							//oracle java altyapısı ile çalışır
 	{
 		public override void Delete()
 		{
