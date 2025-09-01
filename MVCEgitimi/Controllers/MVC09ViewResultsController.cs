bc671506a04e22a1cc640effcb3d2863cecbbdb2
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-02 01:20:03 +0300

    File upload - view result

--- MVCEgitimi/Controllers/MVC09ViewResultsController.cs
+++ MVCEgitimi/Controllers/MVC09ViewResultsController.cs
@@ -1,20 +1,71 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCEgitimi.Models;
 
 namespace MVCEgitimi.Controllers
 {
 	public class MVC09ViewResultsController : Controller
 	{
+		private readonly UyeContext _context;
+
+		public MVC09ViewResultsController(UyeContext context)
+		{
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
 		}
-		public IActionResult FarkliViewDondur()
+
+		public IActionResult FarkliViewDondur() //action
 		{
 			return View("Index"); //ındex isimli ekranı döndürsün denilebilir
 		}
 		public IActionResult Yonlendir()
 		{
-			return Redirect("/Home");
+			//return Redirect("/Home"); //kullanıcıyı bu adrese yönlendir
+			return Redirect("https://www.google.com/ome"); //işlem sonunda kullanıcıyı başka bir sitede de yönlendirebiliriz
+		}
+
+		public IActionResult ActionaYonlendir()
+		{
+			//return RedirectToAction("Index");
+			//return RedirectToAction("Yonlendir"); //Yonlendir action ı tetikle
+			return RedirectToAction("Index", "MVC06CRUD"); //action ve controller 
+		}
+
+		public RedirectToRouteResult RouteYonlendir()
+		{
+			return RedirectToRoute("Default", new { controller = "Home", action = "Index", id = 18 });
+		}
+
+		public PartialViewResult KategorileriGetirPartial() //action
+		{
+			return PartialView("_PartialMenu"); //ındex isimli ekranı döndürsün denilebilir
+		}
+
+		public JsonResult JsonDondur() //action
+		{
+			var kullanicilar = _context.Uyeler.ToList();
+			return Json(kullanicilar); //ındex isimli ekranı döndürsün denilebilir
+		}
+
+		public ContentResult XmlContentResult() //action
+		{
+			var kullanicilar = _context.Uyeler.ToList();
+			var xml = "<kullanicilar>";
+			foreach (var item in kullanicilar)
+			{
+				xml += $@"<kullanici>
+					   <Id>{item.Id}</Id>
+					   <Ad>{item.Ad}</Ad>
+					   <Soyad>{item.Soyad}</Soyad>
+					   <TcKimlikNo>{item.TcKimlikNo}</TcKimlikNo>
+					   <DogumTarihi>{item.DogumTarihi}</DogumTarihi>
+                       </kullanici>";
+			}
+			xml += "</kullanicilar>";
+			return Content(xml, "application/xml"); //ındex isimli ekranı döndürsün denilebilir
 		}
 	}
 }
---
+++ MVCEgitimi/Controllers/MVC10FileUploadController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCEgitimi.Controllers
+{
+	public class MVC10FileUploadController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(IFormFile? dosya) //? null gelebilir
+			//Mvc de dosya yükleme IFormFile interface i ile yapılıyor. Burada dosya isminin ekrandaki file upload name i ile aynı olması gerekir yoksa dosya yüklenmez!
+		{
+			if (dosya != null)
+			{
+				var uzanti = Path.GetExtension(dosya.FileName); //yüklenecej dosya uzantısı
+				string klasor = Directory.GetCurrentDirectory() + "/wwwroot/Images/";
+				var klasorVarmi = Directory.Exists(klasor);
+				TempData["Message"] = "klasorVarmi: "  + klasorVarmi;
+				if (klasorVarmi == false) //eğer sunucuda bu konumda klasör yoksa
+				{
+					var sonuc = Directory.CreateDirectory(klasor); //ana dizine Images klasörü oluştur
+					TempData["Message"] += " - Klasör Oluşturuldu.. " + sonuc;
+				}
+				if (uzanti == ".jpg" || uzanti == ".jpeg" || uzanti == ".png" || uzanti == ".gif") //sadece bu uzantılardaki dosyaları kabul et
+				{
+					//1.Yöntem Random (Rastgele) İsimle Dosya Yükleme
+					var randomFileName = Path.GetRandomFileName(); //rastgele dosya ismi oluşturma metodu
+					var fileName = Path.ChangeExtension(randomFileName, ".jpg"); //dosya adı ve uzantısını değiştirip birleştirdik
+					var path = Path.Combine(klasor, fileName); //klasör ve resim adını birleştirdik
+					using var stream = new FileStream(path, FileMode.Create); //resmi sunucuya yükledik
+					TempData["Resim"] = dosya.FileName;
+				}
+				else
+				{
+					TempData["Message"] += "Sadece Resim Dosyası Yükleyebilirsiniz!";
+				}
+			}
+			return View();
+		}
+	}
+}
