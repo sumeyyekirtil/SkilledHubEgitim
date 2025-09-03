5e539bfff7b841f07a33754f9c25553f5ec676a0
sumeyye <sumeyyekirtil4@gmail.com>
2025-09-03 23:59:23 +0300

    sql komutları

--- WindowsForms/SqlEgitim/SQLQuery4.sql
+++ WindowsForms/SqlEgitim/SQLQuery4.sql
@@ -171,6 +171,7 @@ UNION --her select deyimi ayný satýra sahip olacak,tekrar eden kayýtlar engellen
 SELECT City FROM Suppliers
 ORDER BY City;
 
+
 SELECT City FROM Customers
 UNION All --tekrar eden kayýtlar için all kullanýlýr
 SELECT City FROM Suppliers
