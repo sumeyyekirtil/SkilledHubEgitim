39697ba3d25345afcda973edd8582389fcd7cfbb
sumeyye <sumeyyekirtil4@gmail.com>
2025-10-03 23:49:39 +0300

    mvc razor syntax

--- MVCEgitimi/Controllers/MVC01RazorSyntaxController.cs
+++ MVCEgitimi/Controllers/MVC01RazorSyntaxController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MVCEgitimi.Controllers
-{//yeni controller ekleme project controller - sağ tık - add controller - empty - name - add
+{
+	//yeni controller ekleme project controller - sağ tık - add controller - empty - name - add
 	//varsayılan sayfa aksi yazılmaz ise index tir
 	public class MVC01RazorSyntaxController : Controller
 	{
-		public IActionResult Index()
+		public IActionResult Index() //add - empty razor
 		{
 			return View();
 		}
