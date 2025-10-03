2fbfab2f99f1aa8dab38bbbb70e06ca8fbebf3b3
sumeyye <sumeyyekirtil4@gmail.com>
2025-10-04 01:39:29 +0300

    added is Html Tag Helpers

--- MVCEgitimi/Controllers/MVC02HtmlAndTagHelpersController.cs
+++ MVCEgitimi/Controllers/MVC02HtmlAndTagHelpersController.cs
@@ -2,6 +2,8 @@
 
 namespace MVCEgitimi.Controllers
 {
+	//HtmlAndTagHelpers : razor syntax da html elementlerini oluşturmak için yardımcı olan metotlar
+	//tag helpers: form - a elementlerinde kullanılır asp- ler
 	public class MVC02HtmlAndTagHelpersController : Controller
 	{
 		public IActionResult Index() //indexe view
